6185006419576493e8c9ea8d39ed66c614d61f5b
MikeJConner <mikeJconner6@gmail.com>
2020-02-01 12:58:15 -0800

    Added some JS to make images turn to gifs when hovered over

---
+++ resources/index.js
@@ -0,0 +1,6 @@
+function startSnake() {
+  document.getElementById("hoverGif").src = "resources/images/snake.gif";
+}
+function stopSnake(){
+  document.getElementById("hoverGif").src="resources/images/snake.png";
+}
