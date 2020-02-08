32f71f35edb8c8ec1eb327f37c1b6956e7ed4e9d
MikeJConner <mikeJconner6@gmail.com>
2020-02-07 19:12:45 -0800

    starting to try and add a photo slideshow

--- resources/index.js
+++ resources/index.js
@@ -1,6 +1,38 @@
+//functions for animation of the images in my portfolio when hovered over
 function startSnake() {
   document.getElementById("hoverGif").src = "resources/images/snake.gif";
 }
 function stopSnake(){
   document.getElementById("hoverGif").src="resources/images/snake.png";
+}
+
+
+//Javascript for the Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
