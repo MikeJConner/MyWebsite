461a3e71d3dd1ff912c6dc11e0d0a30862fcf9c8
MikeJConner <mikejconner6@gmail.com>
2020-02-09 19:12:44 -0800

    changed methods for slideshow

--- resources/index.js
+++ resources/index.js
@@ -7,32 +7,24 @@ function stopSnake(){
 }
 
 
-//Javascript for the Slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+var slides = document.getElementsByClassName("slide");
+console.log("slides:");
+console.log(slides);
+var currentSlide = 0;
+var leaving = slides[currentSlide];
+var entering;
+var slideInterval = setInterval(nextSlide,2000);
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+function nextSlide() {
+  leaving.classList.remove("leaving");
+
+  leaving = slides[currentSlide];
+  currentSlide = (currentSlide+1)%slides.length;
+  entering = slides[currentSlide];
+
+  leaving.classList.add("leaving");
+  leaving.classList.remove("showing");
+  entering.classList.add("showing");
 }
