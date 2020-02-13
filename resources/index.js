3be29909819c21cd7a9a6760390adb8f49a73297
MikeJConner <mikejconner6@gmail.com>
2020-02-13 13:22:44 -0800

    made progress on slideshow

--- resources/index.js
+++ resources/index.js
@@ -10,21 +10,23 @@ function stopSnake(){
 
 
 var slides = document.getElementsByClassName("slide");
-console.log("slides:");
-console.log(slides);
 var currentSlide = 0;
 var leaving = slides[currentSlide];
 var entering;
 var slideInterval = setInterval(nextSlide,2000);
 
-function nextSlide() {
-  leaving.classList.remove("leaving");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function nextSlide() {
 
   leaving = slides[currentSlide];
   currentSlide = (currentSlide+1)%slides.length;
   entering = slides[currentSlide];
-
   leaving.classList.add("leaving");
   leaving.classList.remove("showing");
   entering.classList.add("showing");
+  await sleep(1000);
+  leaving.classList.remove("leaving");
 }
